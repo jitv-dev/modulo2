aa36dbde07e1d64b07b23276768a2687457052fb
Javier Torres <javier.torresvalenz@gmail.com>
2025-07-04 23:29:20 -0400

    pagina consejos, falta llenar modulo

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,35 @@ $(document).ready(function () {
 
 
 });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+
+
+document.querySelector('form').addEventListener('submit', function (e) {
+    const email1 = document.getElementById('email1').value;
+    const email2 = document.getElementById('email2').value;
+
+    if (email1 !== email2) {
+        e.preventDefault(); // Detiene el envío
+        document.getElementById('validacion-correo').style.display = 'block';
+    }
+});
